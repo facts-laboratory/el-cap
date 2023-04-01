d4fc4f4e8d1c2597a3aad752558b8229ddbcb5a5
Star Dev <91700749+kanbei0605@users.noreply.github.com>
2023-04-01 17:50:07 +0900

    updated bitcoin info

--- packages/coin/src/lib/coin.tsx
+++ packages/coin/src/lib/coin.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
-import redstone from 'redstone-api';
 
 import { mapStateToProps } from '@el-cap/store';
 import { connect } from 'react-redux';
 import { PriceData } from 'redstone-api/lib/types';
+import { ArrowUpIcon, WatchlistIcon } from '../assets/icons';
+import BitcoinSVG from '../assets/svg/bitcoin.svg';
+import GrayButton from '../assets/component/GrayButton';
 
 interface CoinProps {
   goToFeed: () => void;
@@ -13,53 +15,127 @@ interface CoinProps {
 
 export function Coin(props: CoinProps) {
   const { goToFeed, entity, ticker } = props;
-  const [data, setData] = useState<PriceData | undefined>();
   const [error, setError] = useState<string | undefined>();
-
-  useEffect(() => {
-    if (entity) {
-      setData(entity);
-      return;
-    }
-    redstone
-      .getPrice([ticker.toUpperCase()])
-      .then((item: { [token: string]: PriceData }) => {
-        setData(item[ticker.toUpperCase()]);
-      })
-      .catch((e) => {
-        console.log(e);
-        setError(`Sorry, we couldn't find that coin.`);
-      });
-  }, [entity, ticker]);
 
   if (error) return <p>{error}</p>;
 
   return (
     <div className="bg-gray-100 min-h-screen">
-      <div className="container mx-auto px-4 py-10">
-        <h1 className="text-4xl font-bold mb-8 text-center">Coin Page</h1>
-
-        <button
-          className="text-blue-600 hover:text-blue-800 transition-colors duration-200 focus:outline-none mb-8"
-          onClick={() => goToFeed()}
-        >
-          Go to Feed
-        </button>
-
-    {console.log(data)}
-          
-        {data && (
-          <div className="bg-white p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-semibold mb-4">{data.symbol}</h2>
-            <p className="text-lg mb-2">Current Price: {data.value} {data.currency}</p>
-            <p className="text-lg mb-2">Timestamp: {new Date(data.timestamp).toLocaleString()}</p>
-          </div>
-        )}
+      <div className="flex">
+        <div className="text-gray-400 hover:text-gray-600 cursor-pointer font-medium text-sm py-2 inline-flex mr-2">
+          Cryptocurrencies &gt;
+        </div>
+        <div className="text-gray-400 hover:text-gray-600 cursor-pointer font-medium text-sm py-2 inline-flex mr-2">
+          Coins &gt;
+        </div>
+        <div className="text-black hover:text-gray-600 cursor-pointer font-medium text-sm py-2 inline-flex mr-2">
+          Bitcoin
         </div>
       </div>
+      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-8 gap-y-4">
+        <div className="col-span-1">
+          <div className="flex items-center my-4">
+            <img className="w-10 mr-2" src={BitcoinSVG} alt="bitcoin" />
+            <span className="font-bold text-lg mr-2">Bitcoin</span>
+            <GrayButton text="BTC" />
+            <WatchlistIcon className="ml-2" width={18} height={18} />
+          </div>
+          <div className="flex flex-wrap gap-4">
+            <GrayButton text="Rank #1" active={true} />
+            <GrayButton text="Coin" />
+            <GrayButton text="Been Favorited +4.2 Million Times" />
+            <GrayButton text="Website" />
+            <GrayButton text="Explorers" />
+            <GrayButton text="White Paper" />
+            <GrayButton text="Source Code" />
+          </div>
+          <p className="my-2">Tages</p>
+          <div className="flex gap-4 flex-wrap">
+            <GrayButton text="PoW" />
+            <GrayButton text="SHA-256" />
+            <GrayButton text="Store Of Value" />
+            <GrayButton text="Mineable" />
+            <span className="text-blue-500 hover:cursor-pointer">View All</span>
+          </div>
+        </div>
+        <div className="md:col-span-2 col-span-1">
+          <div className="font-bold flex items-center mb-8">
+            <span className="md:text-[60px] text-3xl mr-2 p-2">$28,013.46</span>
+            <span className="p-2 text-white bg-green-500 rounded-2xl flex items-center md:text-2xl text-md">
+              <ArrowUpIcon
+                className="mt-2"
+                color="white"
+                width={15}
+                height={15}
+              />
+              0.02%
+            </span>
+          </div>
+          <div className="grid lg:grid-cols-4 grid-cols-2 gap-2">
+            <div className="flex-col">
+              <span className="text-[#7D7D7D]">Market Cap</span>
+              <br />
+              <span className="font-bold">$535,170,972,845</span>
+              <span className="flex items-center text-green-500">
+                <ArrowUpIcon
+                  className="mt-2 mr-1"
+                  color="green"
+                  width={15}
+                  height={15}
+                />
+                0.02%
+              </span>
+            </div>
+            <div className="flex-col">
+              <span className="text-[#7D7D7D]">Fully Diluted Market Cap</span>
+              <br />
+              <span className="font-bold">$535,170,972,845</span>
+              <span className="flex items-center text-green-500">
+                <ArrowUpIcon
+                  className="mt-2 mr-1"
+                  color="green"
+                  width={15}
+                  height={15}
+                />
+                0.02%
+              </span>
+            </div>
+            <div className="flex-col">
+              <span className="text-[#7D7D7D]">Volume</span>
+              <br />
+              <span className="font-bold">$25,170,972,845</span>
+              <span className="flex items-center text-green-500">
+                <ArrowUpIcon
+                  className="mt-2 mr-1"
+                  color="green"
+                  width={15}
+                  height={15}
+                />
+                0.02%
+              </span>
+            </div>
+            <div className="flex-col">
+              <span className="text-[#7D7D7D]">Circulating Supply</span>
+              <br />
+              <div className="flex justify-between">
+                <span className="font-bold">19,327,200 BTC</span>
+                <span className="font-bold">92%</span>
+              </div>
+              <span className="flex items-center text-green-500">
+                <div className="w-full bg-gray-400 rounded-full h-2.5 dark:bg-gray-700">
+                  <div
+                    className="bg-gray-700 h-2.5 rounded-full"
+                    style={{ width: '92%' }}
+                  ></div>
+                </div>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
-
 
 export default Coin;
 export const ConnectedCoin = connect(mapStateToProps, (dispatch) => ({
