eab322dd2760d179d6812a6d8922092b919184cd
Star Dev <91700749+kanbei0605@users.noreply.github.com>
2023-04-05 20:27:09 +0900

    added showcase button

--- packages/feed/src/lib/feed.tsx
+++ packages/feed/src/lib/feed.tsx
@@ -42,34 +42,54 @@ export interface FeedProps {
 
 export function Feed(props: FeedProps) {
   const { goToCoin } = props;
+  const [showCase, setShowCase] = useState<boolean>(true);
 
   return (
     <div className="min-h-[calc(100vh-217px)]">
       <div className="mx-auto p-4">
-        <div className="mb-8">
-          <p className="text-black font-bold text-3xl">
-            Today's Cryptocurrency Prices by Market Cap
-          </p>
-          <p className="text-gray-500">
-            The global market cap is{' '}
-            <span className="text-green-500 font-bold">$1.18T,</span>a
-            <span className="text-green-500 font-bold">+1.38%</span> increase
-            over the last day. <span className="text-gray-400">Read More</span>
-          </p>
+        <div className="mb-8 flex justify-between items-center">
+          <div>
+            <p className="text-black font-bold text-3xl">
+              Today's Cryptocurrency Prices by Market Cap
+            </p>
+            <p className="text-gray-500">
+              The global market cap is{' '}
+              <span className="text-green-500 font-bold">$1.18T,</span>a
+              <span className="text-green-500 font-bold">+1.38%</span> increase
+              over the last day.{' '}
+              <span className="text-gray-400">Read More</span>
+            </p>
+          </div>
+          <div className="flex items-center">
+            <span className="text-sm font-medium text-gray-900 mr-2 dark:text-gray-300">
+              Showcase
+            </span>
+            <label className="relative inline-flex items-center cursor-pointer">
+              <input type="checkbox" value="" className="sr-only peer" />
+              <div
+                className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300  rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-teal-500"
+                onClick={() => setShowCase(!showCase)}
+              ></div>
+            </label>
+          </div>
         </div>
-        <div className="flex flex-wrap gap-5 my-6">
-          {data.map((val, key) => {
-            return (
-              <div className="flex-1" key={key}>
-                <WidgetCoinCard
-                  title={val.title}
-                  type={val.type}
-                  data={val.data}
-                />
-              </div>
-            );
-          })}
-        </div>
+        {showCase ? (
+          <div className="flex flex-wrap gap-5 my-6">
+            {data.map((val, key) => {
+              return (
+                <div className="flex-1" key={key}>
+                  <WidgetCoinCard
+                    title={val.title}
+                    type={val.type}
+                    data={val.data}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <></>
+        )}
         <div className="flex justify-between items-center">
           <TabComponent />
           <DropDownAllTypes />
