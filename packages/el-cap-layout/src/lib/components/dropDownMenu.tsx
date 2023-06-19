8c4887e1eab4f231a649e58a33bcc815f68d929c
Star Dev <91700749+kanbei0605@users.noreply.github.com>
2023-06-20 04:38:40 +0900

    feat: updated drop-down-menu shadow

--- packages/el-cap-layout/src/lib/components/dropDownMenu.tsx
+++ packages/el-cap-layout/src/lib/components/dropDownMenu.tsx
@@ -62,11 +62,11 @@ class DropDownMenu extends Component<DropDownMenuProps> {
           <div className="mx-auto flex items-center justify-center">
             <div className="group relative cursor-pointer">
               <div className="flex items-center justify-between space-x-5 bg-white px-4">
-                <div className="menu-hover py-2 text-2xl font-bold text-black lg:mx-2">
+                <div className="menu-hover text-2xl font-bold text-black lg:mx-2">
                   {label}
                 </div>
               </div>
-              <div className="grid grid-cols-2 min-w-[40rem] invisible text-lg absolute left-1/2 transform -translate-x-1/2 group-hover:visible bg-white rounded-lg shadow-xl p-8">
+              <div className="grid grid-cols-2 min-w-[40rem] invisible text-lg absolute left-1/2 transform -translate-x-1/2 group-hover:visible bg-white rounded-lg shadow-xl p-8 top-12">
                 <div>{groupMenu(groupedOptions.slice(0, 2))}</div>
                 <div>{groupMenu(groupedOptions.slice(2, 4))}</div>
               </div>
@@ -76,11 +76,11 @@ class DropDownMenu extends Component<DropDownMenuProps> {
           <div className="mx-auto flex items-center justify-center">
             <div className="group relative cursor-pointer">
               <div className="flex items-center justify-between space-x-5 bg-white px-4">
-                <div className="menu-hover py-2 text-2xl font-bold text-black lg:mx-2">
+                <div className="menu-hover text-2xl font-bold text-black lg:mx-2">
                   {label}
                 </div>
               </div>
-              <div className="invisible text-lg absolute left-1/2 transform -translate-x-1/2 group-hover:visible min-w-[10rem] bg-white rounded-lg shadow-xl p-4">
+              <div className="invisible text-lg absolute left-1/2 transform -translate-x-1/2 group-hover:visible min-w-[10rem] bg-white rounded-lg shadow-xl p-4 top-12">
                 {groupMenu(groupedOptions)}
               </div>
             </div>
