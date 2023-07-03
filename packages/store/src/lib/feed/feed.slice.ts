9750babce57fd69277483d2e3af5ede224dbd9ad
Luke Annison <lukeannison@gmail.com>
2023-07-03 14:04:13 +0100

    fix: bug of not showing feed when it failed

--- packages/store/src/lib/feed/feed.slice.ts
+++ packages/store/src/lib/feed/feed.slice.ts
@@ -49,47 +49,62 @@ export const feedAdapter = createEntityAdapter<FeedEntity>({
 export const fetchFeed = createAsyncThunk(
   'feed/fetchFeed',
   async (key: string, thunkAPI) => {
+    console.log('fetchFeed: Start');
     try {
       const { feed } = thunkAPI.getState() as RootState;
+      console.log('fetchFeed: Got RootState', feed);
       const { entities } = feed;
 
       if (Object.keys(entities).length === 0) {
+        console.log('fetchFeed: Entities is empty');
         const prices = await getPrices();
-        console.log('prices in feed', prices);
+        console.log('fetchFeed: Prices fetched', prices);
         if (Object.keys(prices.remaining).length > 0) {
-          // Run this if there is no remaining data
+          console.log('fetchFeed: Prices.remaining is not empty');
           const combinedPrices = mergeObjects(
             prices.redstone,
             prices.remaining
           );
+          console.log('fetchFeed: CombinedPrices', combinedPrices);
 
           const processedPrices = await processTokenData(combinedPrices);
-          // Get the first 30 keys and values
+          console.log('fetchFeed: ProcessedPrices', processedPrices);
           const first30ProcessedPricesArray = Object.keys(processedPrices)
             .slice(0, 30)
             .map((key) => processedPrices[key]);
-
-          const state = await getLastUpdatedState();
+          console.log(
+            'fetchFeed: First30ProcessedPricesArray',
+            first30ProcessedPricesArray
+          );
 
           if (await isLastUpdatedOverDay()) {
+            console.log('fetchFeed: Last updated over a day ago');
             updateCoinsRecursive(first30ProcessedPricesArray);
+          } else {
+            console.log('fetchFeed: Last updated within a day');
           }
 
           const sortedPrices = sortPrices(entriesToObj(processedPrices), key);
-          console.log('ready to return in feed', sortedPrices);
-
+          console.log('fetchFeed: SortedPrices', sortedPrices);
+          console.log('fetchFeed: Ready to return sorted prices');
           return sortedPrices;
         } else {
-          console.log('prices.redstone', prices.redstone);
-          return prices.redstone;
+          console.log('fetchFeed: Prices.remaining is empty');
+          const state = await getLastUpdatedState();
+          console.log('fetchFeed: LastUpdatedState', state);
+          console.log('fetchFeed: Returning prices.redstone');
+          return state.coins;
         }
       } else {
-        // If entities exist, sort them and also add watchlist flag
-        console.log('returning with entities');
-        return sortPrices(entriesToObj(entities), key);
+        console.log('fetchFeed: Entities exist');
+        const sortedEntities = sortPrices(entriesToObj(entities), key);
+        console.log('fetchFeed: SortedEntities', sortedEntities);
+        console.log('fetchFeed: Returning sorted entities');
+        return sortedEntities;
       }
     } catch (error) {
-      console.log('failing', error);
+      console.log('fetchFeed: Error', error);
+      console.log('fetchFeed: Returning empty array');
       return [];
     }
   }
